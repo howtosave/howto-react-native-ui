d4bf68e6b9e560249ce1463e3bb02eb46c9388b2
peterhs.kang <peterhs.kang@gmail.com>
2022-11-09 19:24:17 +0900

    add tailwindcss for web

--- app-main/App.js
+++ app-main/App.js
@@ -1,14 +1,18 @@
+import './styles/globals.css';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app~</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
+
+//<View className="flex-1 items-center justify-center bg-white">
+
 //     <View style={styles.container}>
 // const styles = StyleSheet.create({
 //   container: {
