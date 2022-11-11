e94e0f13cb6ef422d8e5ad17e88440f4ec6a4be3
peterhs.kang <peterhs.kang@gmail.com>
2022-11-12 04:27:40 +0900

    add ref-webview

---
+++ ref-webview/src/web-view.tsx
@@ -0,0 +1,81 @@
+/**
+ * See https://docs.expo.dev/versions/v47.0.0/sdk/webview/
+ */
+import * as React from 'react';
+import { WebView } from 'react-native-webview';
+import { StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+
+const _js = `
+const labels = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+];
+const data = {
+  labels: labels,
+  datasets: [{
+    label: 'My First dataset',
+    backgroundColor: 'rgb(255, 99, 132)',
+    borderColor: 'rgb(255, 99, 132)',
+    data: [0, 10, 5, 2, 20, 30, 45],
+  }]
+};        
+const config = {
+  type: 'line',
+  data: data,
+  options: {}
+};        
+
+const myChart = new Chart(
+  document.getElementById('myChart'),
+  config,
+  data,
+);
+true; // note: this is required, or you'll sometimes get silent failures
+`;
+const _html = `
+<head>
+<meta name="viewport" content="width=device-width, user-scalable=no" />
+</head>
+<body>
+  <div>
+    <p>hi</p>
+    <canvas id="myChart"></canvas>
+  </div>
+  <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+</body>
+</html>
+`;
+
+export default function WebViewScreen() {
+  const refWebView = React.useRef<WebView>();
+
+  setTimeout(() => {
+    const run = `document.body.style.backgroundColor = 'blue';`;
+    refWebView.current && refWebView.current.injectJavaScript(run);
+  }, 3000);
+
+  return (
+    <WebView
+      ref={refWebView}
+      style={styles.container}
+      originWhitelist={['*']}
+      // source={{ html: '<h1><center>Hello world</center></h1>' }}
+      // source={{ uri: 'https://expo.dev' }}
+      source={{ html: _html }}
+      injectedJavaScript={_js}
+      onMessage={(event) => {console.log('[RECV]', event);}}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: Constants.statusBarHeight,
+  },
+});
