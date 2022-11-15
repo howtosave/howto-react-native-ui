6e1f34e48aab31042b9a704db3b1f9eef84ebe92
peterhs.kang <peterhs.kang@gmail.com>
2022-11-15 14:22:45 +0900

    add react-navigation example

---
+++ expo-react-navigation/babel.config.js
@@ -0,0 +1,13 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+
+      // Needed by React Native Reanimated for web
+      '@babel/plugin-proposal-export-namespace-from',
+      // !!! Reanimated plugin has to be listed last.
+      'react-native-reanimated/plugin'
+    ],
+  };
+};
---
+++ expo-react-navigation/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const fs = require('fs');
+const createExpoWebpackConfigAsync = require('@expo/webpack-config');
+
+const node_modules = path.resolve(__dirname, '..', 'node_modules');
+
+module.exports = async function (env, argv) {
+  const config = await createExpoWebpackConfigAsync(env, argv);
+
+  // config.module.rules.push({
+  //   test: /\.(js|ts|tsx)$/,
+  //   include: /(packages|example)\/.+/,
+  //   exclude: /node_modules/,
+  //   use: 'babel-loader',
+  // });
+
+  // Object.assign(config.resolve.alias, {
+  //   'react': path.resolve(node_modules, 'react'),
+  //   'react-native': path.resolve(node_modules, 'react-native-web'),
+  //   'react-native-web': path.resolve(node_modules, 'react-native-web'),
+  //   '@expo/vector-icons': path.resolve(node_modules, '@expo/vector-icons'),
+  // });
+
+  return config;
+};
