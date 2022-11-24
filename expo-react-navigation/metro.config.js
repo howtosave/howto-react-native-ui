367b5c8a436857c3a5fd73c1e327c4d8a522bbdc
peterhs.kang <peterhs.kang@gmail.com>
2022-11-24 11:58:16 +0900

    upate expo-react-navigation example

--- expo-react-navigation/metro.config.js
+++ expo-react-navigation/metro.config.js
@@ -7,22 +7,39 @@ const { getDefaultConfig } = require('@expo/metro-config');
 const exclusionList = require('metro-config/src/defaults/exclusionList');
 
 const root = path.resolve(__dirname, '..');
-// const packages = path.resolve(root, 'packages');
+const packages = path.resolve(root, 'packages');
 
 const defaultConfig = getDefaultConfig(__dirname);
 
 // List all packages under `packages/`
-// const workspaces = fs
-//   .readdirSync(packages)
-//   .map((p) => path.join(packages, p))
-//   .filter(
-//     (p) =>
-//       fs.statSync(p).isDirectory() &&
-//       fs.existsSync(path.join(p, 'package.json'))
-//   );
+const workspaces = fs
+  .readdirSync(packages)
+  .map((p) => path.join(packages, p))
+  .filter(
+    (p) =>
+      fs.statSync(p).isDirectory() &&
+      fs.existsSync(path.join(p, 'package.json'))
+  );
 
 // Get the list of dependencies for all packages in the monorepo
 const modules = ['@expo/vector-icons']
+  // .concat(
+  //   ...workspaces.map((it) => {
+  //     const pak = JSON.parse(
+  //       fs.readFileSync(path.join(it, 'package.json'), 'utf8')
+  //     );
+
+  //     // We need to make sure that only one version is loaded for peerDependencies
+  //     // So we exclude them at the root, and alias them to the versions in example's node_modules
+  //     return pak.peerDependencies ? Object.keys(pak.peerDependencies) : [];
+  //   })
+  // )
+  // .sort()
+  // .filter(
+  //   (m, i, self) =>
+  //     // Remove duplicates and package names of the packages in the monorepo
+  //     self.lastIndexOf(m) === i && !m.startsWith('@react-navigation/')
+  // );
 
 module.exports = {
   ...defaultConfig,
