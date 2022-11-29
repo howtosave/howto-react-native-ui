68211c16140fb84c2052eb880d7a3a8c9ebaf0d2
peterhs.kang <peterhs.kang@gmail.com>
2022-11-29 20:30:09 +0900

    add hardwareBackPress handler

--- expo-react-navigation/src/drawer-with-stack/Home.tsx
+++ expo-react-navigation/src/drawer-with-stack/Home.tsx
@@ -1,7 +1,9 @@
 import type { DrawerScreenProps } from '@react-navigation/drawer';
+import { useFocusEffect } from '@react-navigation/native';
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import { Button } from 'react-native-paper';
+import { BackHandler, StyleSheet, View } from 'react-native';
+import { Button, Divider } from 'react-native-paper';
+import Toast from 'react-native-root-toast';
 
 import Contacts from '../Shared/Contacts';
 import type { RootDrawerParamList } from './pages';
@@ -9,11 +11,36 @@ import type { RootDrawerParamList } from './pages';
 export default function HomePage({
   navigation,
 }: DrawerScreenProps<RootDrawerParamList>) {
+  const [backPressed, setBackPressed] = React.useState(false);
+
+  //
+  // hardwareBackPress handler
+  useFocusEffect(
+    React.useCallback(() => {
+      const onBackPress = () => {
+        if (backPressed) {
+          return false;
+        } else {
+          setBackPressed(true);
+          Toast.show('종료하시려면 `뒤로가기` 버튼을\n한번 더 클릭해주세요', {
+            duration: 2000,
+          });
+          setTimeout(() => setBackPressed(false), 1900);
+          return true;
+        }
+      };
+
+      const subscription = BackHandler.addEventListener(
+        'hardwareBackPress',
+        onBackPress
+      );
+
+      return () => subscription.remove();
+    }, [backPressed])
+  );
+
   return (
     <View style={styles.container}>
-      <Button onPress={() => navigation.navigate('Stacked')}>
-        goto Stacked
-      </Button>
       <Button
         onPress={() =>
           navigation.navigate('Tabbed', {
@@ -23,6 +50,12 @@ export default function HomePage({
         }
       >
         goto TabbedB in Tabbed
+      </Button>
+
+      <Divider />
+
+      <Button onPress={() => navigation.navigate('Stacked')}>
+        goto Stacked
       </Button>
 
       <Contacts />
